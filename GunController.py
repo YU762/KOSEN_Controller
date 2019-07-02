790f7eb2aace9c567a2c5f940250e33284569b47
YU762 <akunya101@icloud.com>
2019-07-02 11:38:27 +0900

    通信部分の追加

--- GunController.py
+++ GunController.py
@@ -1,7 +1,13 @@
 #!/usr/biin/env python
 
 import RPI.GPIO as GPIO
+import socket
 #import time
+
+HOST = "0.0.0.0"
+PORT = 22
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup()
@@ -13,14 +19,16 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 try:
 	while True:
 		if GPIO.input(8) == GPIO.HIGH:
-		
-			#Nodeに渡す処理
+                        result = str(a)
+                        print(result)
+                        client.sentto(result.encode('utf-8'),(HOST,PORT))
 		
 		elif GPIO.input(8) == GPIO.HIGH:
+                        result = str(b)
+                        print(result)
+                        client.sentto(result.encode('utf-8'),(HOST,PORT))
 		
-			##Nodeに渡す処理
-		
-		#time.sleepp(0.1)
+		time.sleep(0.1)
 		
 except KeyboardInterrupt:
 	pass
